e2485a4d7ff5003a32bc9e2199785d6b35361524
bunnychals <156400338+bunnychals@users.noreply.github.com>
2024-01-12 11:21:46 +0530

    Update bunny.py

--- bunny.py
+++ bunny.py
@@ -1,4 +1,4 @@
-import streamlitas st
+import streamlit as st
 st.header('puppy')
 st.write('puppy')
 st.image('https://www.dailypaws.com/thmb/BFfyDi9uF-s4kbQ31NKmCiUrNVE=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/standing-german-shepherd-151533956-2000-f7799366e61c4a0a8fca2f72671e81f7.jpg',caption='puppy')
